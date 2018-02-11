42b8d4c307b8251c3a62931fa1f24a88ab8c04e0
silaschen <chensiwei1@outlook.com>
2018-02-11 10:45:10 +0800

    beauty

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -130,7 +130,7 @@
                 <span><a href="" class="blog-color">article &nbsp;</a></span>
                 <span> @4w &nbsp;</span>
                 <span>{{date('Y-m-d',122222223131)}}</span>
-                <h1><a href="{{url('read')}}/6">asdasd</a></h1>
+                <h1><a href="{{url('read')}}/`+v.id+`">`+v.title+`</a></h1>
                 <p>我们一直在坚持着，不是为了改变这个世界，而是希望不被这个世界所改变。
                 </p>
                 <p><a href="" class="blog-continue">continue reading</a></p>
