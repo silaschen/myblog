3203dfc67301f754b57a000f9a017b84fdfd6faa
silaschen <chensiwei1@outlook.com>
2018-02-24 11:27:13 +0800

    tpl about

---
+++ resources/views/index/about.blade.php
@@ -0,0 +1,21 @@
+@extends('index.layout')
+@section('content')
+
+
+<div class="am-g am-g-fixed blog-fixed blog-content">
+    <div class="am-u-sm-12 readbox">
+
+    	<h1>About me</h1>
+    	<p>First Welcome to be here.I am a php developer for 2 years.&nbspThis blog site is for all who love php and coding. This web site is placed on AWS ubuntu system. And the webserver is Apache2.The database is used Mysql.</p>
+    	<p>I will place my life story and all the exprinces which I get during developing here.So I hope we can make progress at developing together.</p>
+    	<p>U can contact me via the way below.</p>
+    	<p>QQ:434684326</p>
+    	<p>Wechat:talentchensw</p>
+    	<p>Github:<a href="https://github.com/silaschen" style="color: green">https://github.com/silaschen</a></p>
+    	<p>I'd like to make friends with you all.Let's go for the next step!!!</p>
+
+
+
+    </div>
+</div>
+@endsection
